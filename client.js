ff4dabdf92bb0f3fabf515c78eba7b2dab3b285c
Aak172003 <prajapatiaakash364@gmail.com>
2025-06-03 10:40:35 +0530

    add-notes

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { Kafka } from "kafkajs";
 
 export const kafka = new Kafka({
-    brokers: ["192.168.29.236:9092"],
-    clientId: "kafka-admin",
+  brokers: ["IP_ADDRESS:9092"],
+  clientId: "kafka-admin",
 });
